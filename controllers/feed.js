b89480d5613af8160173de59046376cd0b67c1fc
gaby <gabriel@xmanna.com>
2023-10-21 19:06:53 +0300

    added validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,43 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'My First post', content: 'This is the first post!'}]
+        posts: [
+            {
+                _id: 1,
+                title: 'My First post', 
+                content: 'This is the first post!', 
+                imageUrl: 'images/logo.jpg',
+                creator: {
+                    name: 'Gabriel'
+                },
+                createdAt: new Date()
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res
+                .status(422)
+                .json({
+                    message: 'Validation failed', 
+                    errors: errors.array()
+                });
+    }
     const title = req.body.title;
     const content = req.body.content;
     res.status(201).json({
         message: 'Post created successfully',
-        post: {id: new Date().toISOString(), title:title, content: content}
+        post: {
+            _id: new Date().toISOString(), 
+            title:title, 
+            content: content,
+            creator: { name: 'Gabriel'},
+            createdAt: new Date()
+        }
+
     });
 }
